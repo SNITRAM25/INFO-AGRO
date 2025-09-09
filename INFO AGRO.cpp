80baf1fb8c704fbc42536b09fa4ed4dddd5cd4a8
Snitram <ra145923@uem.br>
2025-09-09 11:10:48 -0300

    Add files via upload

--- INFO AGRO.cpp
+++ INFO AGRO.cpp
@@ -6,16 +6,18 @@
 int main(){
 	
 	setlocale(LC_ALL,"Portuguese"); //identificar idioma
+	
+	printf("pindamonhagaba\n");
 	//declaração de variáveis
-	int idade=0, ano, custo, juros, brasileiro, ano_atual;
-	float altura, valorTotal;
+	int idade, ano, custo, ano_atual;
+	float juros;
 	char status; //está sendo armazenado apenas 1 caracter
 	char nome[20]; //está sendo armazenado uma palavra com 20 caractéres
 	
 	idade = 18;
-	ano = 2015;
+	ano = 20007;
 	ano_atual = ano + idade;
 	custo = 66.89;
-	juros = custo * 1.05;
-	brasileiro = true;
+	juros = 29+3-4/8;
+	printf("%f", juros);
 }
